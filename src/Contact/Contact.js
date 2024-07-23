84ccf1025d3dbd73c67959870a012075358cd00c
goncalo.mendes90 <goncalo.mendes@leanbakery.com>
2024-07-23 19:03:18 +0100

    remove unused links

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -13,20 +13,6 @@ export default function Contact() {
             <p className="text-xs">
               Rua da Boavista 10, Lisbon, Portugal 1200-067
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                E-mail
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Book a table <span aria-hidden="true">→</span>
-              </a>
-            </div>
           </div>
         </div>
       </div>
